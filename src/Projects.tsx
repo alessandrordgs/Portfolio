93e772a85a62a7c6820e432acca716d9a02089d1
forge1010 <ale.silva1120@gmail.com>
2021-07-10 17:21:17 -0300

    sobre mim responsivo

--- src/Projects.tsx
+++ src/Projects.tsx
@@ -7,7 +7,7 @@ export function Projects(){
         <>
         <ProjectsComponent img={siteHome} description="Projeto para alugueis de apartamentos " title="TCC Home" site="https://home-three-taupe.vercel.app" github="https://github.com/alessandrordgs/home"/>
        
-        <ProjectsComponent img={siteAmazing} description="Projeto para uma empresa fictícia que cria gráficos " title="Amazing Graph" site="https://amazing-graph.vercel.app/" github="https://github.com/alessandrordgs/Amazing-graph"/>
+        <ProjectsComponent img={siteAmazing} description="Projeto para uma empresa fictícia que cria gráficos" title="Amazing Graph" site="https://amazing-graph.vercel.app/" github="https://github.com/alessandrordgs/Amazing-graph"/>
        
         <ProjectsComponent img={letmeask} description="Projeto para criar sala de perguntas e respostas" title="Letmeask" site="https://letmeask-32ca4.web.app/" github="https://github.com/alessandrordgs/letmeask-nlw"/>
        
